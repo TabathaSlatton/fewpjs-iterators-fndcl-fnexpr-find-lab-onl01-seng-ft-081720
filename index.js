471afbfdf1676a1b2aa26d9718993070d66e4d8f
Tabatha Slatton <valbatha@gmail.com>
2020-11-22 21:51:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(objArr) {
+  const found = objArr.find(element => element.result === "W")
+  if (found) {
+    return found.year
+  } 
 }
+
